45a69985afcc0627e4a9fae029662d0fc7970026
Evgeny Yuranev <yuranev.ek@gmail.com>
2021-03-22 18:20:55 +0300

    scss(header): basic template and styles of button-cart

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,16 @@
 <template>
   <header class="header">
     <base-logo></base-logo>
+    <button
+      @click.self="addItemToBasket"
+      class="button button--icon header__icon-cart"
+      type="button"
+      :ariaLabel="ariaLabelButtonCart"
+      :data-quantity="calcQuantityOfBasketItems"
+      :class="{
+        'header__icon-cart--quantity': calcQuantityOfBasketItems,
+      }"
+    ></button>
   </header>
 </template>
 
@@ -10,6 +20,29 @@ export default {
   name: "TheHeader",
   components: {
     BaseLogo,
+  },
+  mounted() {
+    this.getBasketItemIdsFromLocalStorage();
+    this.$ls.on("basketItemIds", this.getBasketItemIdsFromLocalStorage);
+  },
+  data() {
+    return {
+      basketItemIds: {},
+    };
+  },
+  computed: {
+    ariaLabelButtonCart() {
+      return `items in the basket`;
+    },
+    calcQuantityOfBasketItems() {
+      return this.basketItemIds ? Object.keys(this.basketItemIds).length : 0;
+    },
+  },
+  methods: {
+    getBasketItemIdsFromLocalStorage() {
+      const savedBasketItemIds = this.$ls.get("basketItemIds");
+      this.$set(this, "basketItemIds", savedBasketItemIds || {});
+    },
   },
 };
 </script>
